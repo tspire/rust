2b9c8295376b98f5215b952633ace3c2f11bb407
Tormod Bahner <tormod.spire@gmail.com>
2026-01-07 16:05:54 +0100

    Initial commit

---
+++ hello_rust/target/debug/deps/hello_rust-555d9736dec924d2.d
@@ -0,0 +1,5 @@
+/Users/tormod/coding/Gemini/antigravity/rust/hello_rust/target/debug/deps/hello_rust-555d9736dec924d2.d: src/main.rs
+
+/Users/tormod/coding/Gemini/antigravity/rust/hello_rust/target/debug/deps/hello_rust-555d9736dec924d2: src/main.rs
+
+src/main.rs:
---
+++ hello_rust/target/debug/hello_rust.d
@@ -0,0 +1,1 @@
+/Users/tormod/coding/Gemini/antigravity/rust/hello_rust/target/debug/hello_rust: /Users/tormod/coding/Gemini/antigravity/rust/hello_rust/src/main.rs
